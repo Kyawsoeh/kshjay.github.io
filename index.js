2a739045f75b3fb461bd092c39fe691c1b463eab
SPYDER <98939270+Kyawsoeh@users.noreply.github.com>
2022-09-27 17:00:26 +0630

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js">
+</script><script type="text/javascript"> 
+var closedModalHashStateId = "#modalClosed"; var openModalHashStateId = "#modalOpen";window.location.hash = closedModalHashStateId; 
+  
+$('#myModal').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal1').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal1').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal2').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal2').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal3').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal3').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal4').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal4').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal5').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal5').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal6').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal6').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal7').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal7').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal8').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal8').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal9').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal9').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal10').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal10').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal11').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal11').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal12').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal12').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal13').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal13').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal14').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal14').on('hide.bs.modal', function(e) { window.history.back(); }); 
+
+$('#myModal15').on('show.bs.modal', function(e) { window.location.hash = openModalHashStateId; });$('#myModal15').on('hide.bs.modal', function(e) { window.history.back(); }); 
+ 
+
+</script>
